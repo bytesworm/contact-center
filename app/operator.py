9b6ee9af8b1f7c3259e867cf4d2ad3b5121452ee
deadshumz <shumzz@bk.ru>
2025-11-08 14:50:04 +0300

    feat: add Operator class

---
+++ app/operator.py
@@ -0,0 +1,25 @@
+from typing import Optional
+from .call import Call
+
+
+class Operator:
+    def __init__(self, operator_id: int):
+        self.id = operator_id
+        self._busy_until: float = 0.0
+        self._current_call: Optional[Call] = None
+    
+    def is_free(self, now: float) -> bool:
+        return now >= self._busy_until
+    
+    def take_call(self, call: Call, now: float):
+        self._current_call = call
+        self._busy_until = now + call.service_time
+    
+    def finish_call(self) -> Optional[Call]:
+        call = self._current_call
+        self._current_call = None
+        return call
+    
+    def get_current_call(self) -> Optional[Call]:
+        return self._current_call
+
